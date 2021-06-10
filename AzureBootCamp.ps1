9d37c96ebff03a1b046c76fe7cd93da230324afe
Shuo-Huang Chen <shuohchen@microsoft.com>
2021-06-10 17:16:06 +0800

    first commit

---
+++ AzureBootCamp.ps1
@@ -0,0 +1,43 @@
+﻿#Get list of loaded module
+Get-Module
+
+#Install required azure commandlet using administrator previleges 
+Install-Module -Name Az -AllowClobber -SkipPublisherCheck
+
+#Import Module
+Import-Module Az
+
+#Connect Azure remotely with multi-factor authentiacation
+Connect-AzAccount -TenantId cdf32132-1c15-4d10-9ea8-8b6009630dc6
+
+#List subscription
+Get-AzSubscription -SubscriptionName 'Visual Studio Enterprise Subscription'
+
+#Select one specific subscription
+Select-AzSubscription -SubscriptionId '0ea55d77-1224-4a86-a888-913c1ed0fb83'
+
+#Get the resource grous
+ Get-AzResourceGroup
+
+#Get AzContext
+Get-AzContext
+
+###Create a Virtual Network ###
+#Check that the resource group exists, if not create it.
+Get-AzResourceGroup -Name shouhchen-rg -ErrorVariable notPresent -ErrorAction SilentlyContinue
+if($notPresent)
+{
+ New-AzResourceGroup -Name shouhchen-rg -Location "Central Us"
+}
+
+#Create a variable for the subnet
+$appsSubnet = New-AzVirtualNetworkSubnetConfig -Name apps -AddressPrefix "10.0.0.0/24"
+$dataSubnet = New-AzVirtualNetworkSubnetConfig -Name data -AddressPrefix "10.0.1.0/24"
+
+#Create Virtual Network
+$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName shouhchen-rg -Location CentralUs -Name contosoads-vnet -AddressPrefix 10.0.0.0/16 -Subnet $appsSubnet,$dataSubnet
+
+#You can check the virtual network address space by typing the below command
+$virtualNetwork.AddressSpace
+$virtualNetwork.Subnets
+
---
+++ CreateVm.ps1
@@ -0,0 +1,75 @@
+﻿#Import the Azure PowerShell Module
+Import-Module -Name Az
+
+#Show Powershell version
+#$PSVersionTable.PSVersion
+
+#get help about certain commandlet
+#Get-Help Get-ChildItem -detailed
+
+#Search object
+#Get-AzVMImagePublisher -Location $loc | Where-Object -Property PublisherName -Contains 'zscaler'
+Get-AzVMImagePublisher -Location $loc | Where-Object {($_.PublisherName -like 'Canonical')}
+
+# After finding the publisher, get their offers
+Get-AzVMImageOffer -Location $loc -PublisherName "Canonical"
+
+# Search for SKUs with the publisher and offer
+Get-AzVMImageSku -Location $loc -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-focal"
+
+# NOW you can search for your image
+Get-AzVMImage -Location $loc -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-focal" -Skus "20_04-lts" | Select-Object -Last 1
+
+#Connect to an Azure account 
+Connect-AzAccount
+
+#List Azure subscriptions
+#Get-AzSubscription
+
+#Define Azure variable for a virtual machine
+$subscription = "46861af6-3496-44f0-8968-11ce30ae6294"
+$VMName = "testvmJapest20210607"
+$OSDiskName = "os-disk"
+$DataDiskName = "data-disk"
+$resourceGroup = "my-test-gp"
+$loc = "japanwest"
+$VMSize = Get-AzVMSize -Location $loc | Where-Object Name -eq "Standard_B1s" #only free vm for the free account
+
+# List Available options for Size parameters 
+#Get-AzVMSize  -Location $loc
+
+#Select the right subscription 
+Select-AzSubscription -SubscriptionId $subscription
+
+#Create AZ VM config instance
+$VMSettings = New-AzVMConfig -VMName $VMName -VMSize $VMSize.Name 
+
+# Set the size of the OS disk.
+# Also set that the OS disk is going to be created from an image
+Set-AzVMOSDisk -VM $VMSettings -Name $OSDiskName -Linux -DiskSizeInGB 80 -CreateOption FromImage
+
+# Add a data disk
+Add-AzVMDataDisk -VM $VMSettings -Name $DataDiskName -DiskSizeInGB 100 -CreateOption Empty -Lun 1
+
+# Get publishers of image you are looking for
+Get-AzVMImagePublisher -Location $loc  | Select-Object -First 10
+
+# Get VM images
+Get-AzVMImage -Location $loc
+
+#Create Azure Credentials
+#$adminCredential= Get-Credential -Message "Enter a username and password for the VM Administrator"
+
+New-AzVm -ResourceGroupName $resourceGroup -Name $vmName  -Credential (Get-Credential) -Location $loc -Image UbuntuLTS -OpenPorts 22
+
+#Get VM information
+$vm = (Get-AzVM -Name $vmName -ResourceGroupName $resourceGroup)
+
+#Get the property information of vm object
+$vm.HardwareProfile
+
+#Get the storage information
+$vm.StorageProfile.OsDisk
+
+#Get IP Address of my vm
+$vm | Get-AzPublicIpAddress

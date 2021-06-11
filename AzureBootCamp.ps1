d4df95937d82e30ca26c0b1c8731a9f6dec76226
shuohchen <85675872+shuohchen@users.noreply.github.com>
2021-06-11 17:17:42 +0800

    Update AzureBootCamp.ps1
    create SQL Server VM and Web Server VM via ARM

--- AzureBootCamp.ps1
+++ AzureBootCamp.ps1
@@ -24,10 +24,12 @@ Get-AzContext
 
 ###Create a Virtual Network ###
 #Check that the resource group exists, if not create it.
-Get-AzResourceGroup -Name shouhchen-rg -ErrorVariable notPresent -ErrorAction SilentlyContinue
+$rsgName = "shouhchen-rg"
+$loc = "Central Us"
+Get-AzResourceGroup -Name $rsgName -ErrorVariable notPresent -ErrorAction SilentlyContinue
 if($notPresent)
 {
- New-AzResourceGroup -Name shouhchen-rg -Location "Central Us"
+ New-AzResourceGroup -Name $rsgName -Location $loc
 }
 
 #Create a variable for the subnet
@@ -35,9 +37,39 @@ $appsSubnet = New-AzVirtualNetworkSubnetConfig -Name apps -AddressPrefix "10.0.0
 $dataSubnet = New-AzVirtualNetworkSubnetConfig -Name data -AddressPrefix "10.0.1.0/24"
 
 #Create Virtual Network
-$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName shouhchen-rg -Location CentralUs -Name contosoads-vnet -AddressPrefix 10.0.0.0/16 -Subnet $appsSubnet,$dataSubnet
+$virtualNetwork = New-AzVirtualNetwork -ResourceGroupName $rsgName -Location $loc -Name contosoads-vnet -AddressPrefix 10.0.0.0/16 -Subnet $appsSubnet,$dataSubnet
 
 #You can check the virtual network address space by typing the below command
 $virtualNetwork.AddressSpace
 $virtualNetwork.Subnets
+
+###Create virtual machine ###
+##SQL Servire VM##
+# Deploy to a resource group with ARM template with inline parameters 
+$arrayParam = @{ "alias" = "neochen"; "adminPassword" = "demo@pass123"}
+New-AzResourceGroupDeployment -ResourceGroupName $rsgName -TemplateFile azuredeploy.sql.json -TemplateParameterObject  $arrayParam
+
+##Web Server VM##
+
+#Create a storage account, Standard_LRS :Locally-redundant storage
+New-AzStorageAccount -ResourceGroupName $rsgName -Name shouhchendsc -Location centralus -SkuName Standard_LRS
+$storageAccount = Get-AzStorageAccount | Select-Object -Last 1
+
+#Create a container for blob files
+$containerName = "dsc"
+New-AzStorageContainer -Name $containerName -Context $storageAccount.Context -Permission blob
+
+#upload a blob
+#az storage blob upload --account-name "<alias>dsc" -c dsc -f web-dsc-config.zip -n web-dsc-config.zip
+Set-AzStorageBlobContent -File "web-dsc-config.zip" -Container $containerName -Blob "web-dsc-config.zip" -Context $storageAccount.Context 
+
+#Retrieve SAS Token
+$context = New-AzStorageContext -StorageAccountName $account_name -StorageAccountKey $account_key
+
+#you can also add other parameter as per your need, like StartTime, ExpiryTime etc.
+$SASToken = New-AzStorageAccountSASToken -Service Blob -ResourceType Service,Container,Object -Permission rw -Context $storageAccount.Context -ExpiryTime "2030-01-01"
+
+#Deploy an Azure Resource Manager Template
+$arrayParam = @{ "alias" = "neochen"; "adminPassword" = "demo@pass123" ; "dscModulesUrl"= "https://shouhchendsc.blob.core.windows.net/dsc/web-dsc-config.zip"+$SASToken}
+New-AzResourceGroupDeployment -ResourceGroupName $rsgName -TemplateFile azuredeploy.web.json -TemplateParameterObject  $arrayParam
 
